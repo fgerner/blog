66339ac6462115696e3e7a9c7724e6f619edbfb1
Fred <fred.gerner@icloud.com>
2020-05-17 00:32:42 +1000

    wip: posts published on home page

--- app.js
+++ app.js
@@ -15,9 +15,36 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
 
+app.get('/', function (req, res) {
+  res.render('home', {
+    startContent: homeStartingContent,
+    posts: posts
+  });
+  console.log(posts);
+})
 
+app.get('/about', function (req, res) {
+  res.render('about', {aboutContent: aboutContent});
+})
 
+app.get('/contact', function (req, res) {
+  res.render('contact', {contactContent: contactContent});
+})
+
+app.get('/compose', function (req, res) {
+  res.render('compose');
+});
+
+app.post('/compose', function (req, res) {
+  const newPost = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(newPost);
+  res.redirect('/');
+});
 
 
 
